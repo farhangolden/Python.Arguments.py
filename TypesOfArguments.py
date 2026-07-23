4346df1063164e8243d58515de80936b23c248fe
farhangolden <farhan.golden.8281@gmail.com>
2026-07-23 06:56:19 +0530

    My Arguments file are modified,This file is modified by student data

--- TypesOfArguments.py
+++ TypesOfArguments.py
@@ -3,3 +3,8 @@ def greeting(username,class_name,class_timing):
 
 greeting("Farhan","Python Class","9:00PM TO 10:00PM")
 greeting("Ali","Python Class","9:00PM TO 10:00PM")
+
+def display_student_data(*para):
+    print(f="the student details are follows{para}")
+
+display_student_data(1,"Sufiyan",[67,90,87],"Mumbai","B.tech")
