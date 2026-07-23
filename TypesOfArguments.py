bbed47a3691ec497d7831739978c6de892e9f876
farhangolden <farhan.golden.8281@gmail.com>
2026-07-23 06:44:44 +0530

    new name is given

---
+++ TypesOfArguments.py
@@ -0,0 +1,5 @@
+def greeting(username,class_name,class_timing):
+    print(f"Hi! {username} welcome to {class_name} and its time is {class_timing}")
+
+greeting("Farhan","Python Class","9:00PM TO 10:00PM")
+greeting("Ali","Python Class","9:00PM TO 10:00PM")
